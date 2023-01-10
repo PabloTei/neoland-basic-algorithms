d515b105919e17f0903f76141f7ab43f60f53328
PabloTei <pteijeirolopez@gmail.com>
2023-01-10 21:04:18 +0100

    iteracion2

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+// ITERACION 1: VARIABLES
+
 // 1.1
  
 const myFavoriteHero = "Hulk";
-console.log(myFavoriteHero);
+
+// 1.2
+
+const x = 50;
+
+// 1.3
+
+const h = 5;
+const y = 10;
+
+// 1.4
+
+const z = h + y;
+
+// ITERACION 2: VARIABLES AVANZADAS
+
+// 1.1
+
+const character = {name: 'Jack Sparrow', age: 10};
+console.log(character);
+
