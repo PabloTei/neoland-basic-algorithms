dc59621940ba4c9e8b5d16d19585a7a29d21aa9c
PabloTei <pteijeirolopez@gmail.com>
2023-01-10 22:07:30 +0100

    iteracion3

--- app.js
+++ app.js
@@ -55,14 +55,43 @@ const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 const newFinalPriceCar1 = car1['basePrice'] + globalBasePrice;
 const newFinalPriceCar2 = car2['basePrice'] + globalBasePrice;
 
-// Eliminamos del objeto el finalPrice y añadimos el newFinalPrice
+// Modificamos los valores finalPrice por newFinalPrice
 car1['finalPrice'] = newFinalPriceCar1;
 car2['finalPrice'] = newFinalPriceCar2;
 
+// Imprimos por pantalla el nuevo objeto con los precios finales
 console.log(car1);
 console.log(car2);
 
+// ITERACION 3: Operadores
 
+// 1.1
+
+console.log(10 * 5);
+
+// 1.2
+
+console.log(10 / 2);
+
+// 1.3
+
+console.log(15 % 9);
+
+// 1.4
+
+const p = 10;
+const j = 5;
+
+const o = p + j;
+console.log(o);
+
+// 1.5
+
+const c = 10;
+const m = 5;
+
+const i = c * m;
+console.log(i);
 
 
 
